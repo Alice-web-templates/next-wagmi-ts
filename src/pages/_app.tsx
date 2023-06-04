5eee77eec9a6c9866d47b6c5554a1e337da6063a
Hachikoi-the-creator <facka72@gmail.com>
2023-06-03 23:45:52 -0500

    web3 done :D

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,83 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '~/styles/globals.css'
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, useAccount, WagmiConfig } from "wagmi";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  goerli,
+  polygonMumbai,
+  optimismGoerli,
+  arbitrumGoerli,
+  polygonZkEvmTestnet,
+} from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { useRouter } from "next/router";
+
+// need to be available in client...
+const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "no-api-key";
+const { chains, provider } = configureChains(
+  [
+    mainnet,
+    goerli,
+    polygon,
+    polygonMumbai,
+    optimism,
+    optimismGoerli,
+    arbitrum,
+    arbitrumGoerli,
+    polygonZkEvmTestnet,
+  ],
+  [alchemyProvider({ apiKey }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My Alchemy DApp",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export { WagmiConfig, RainbowKitProvider };
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const account = useAccount({
+    onConnect({ address, connector, isReconnected }) {
+      if (!isReconnected) router.reload();
+    },
+  });
+
+  const initialChainNum = process.env.NEXT_PUBLIC_DEFAULT_CHAIN
+    ? +process.env.NEXT_PUBLIC_DEFAULT_CHAIN
+    : polygonMumbai.id;
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        modalSize="compact"
+        initialChain={initialChainNum}
+        chains={chains}
+      >
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
+
+export default MyApp;
